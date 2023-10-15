0f83dd15077a5076fe0b901a52113c02d0faab6c
Mike <michaelgross.tn@gmail.com>
2023-10-14 22:17:07 -0400

    Nudes

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ app.use(express.urlencoded({ extended: true }))
 const auth = require('./Auth.js')(app)
 const passport = require('passport');
 require('./Passport.js');
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 //Connect to MongoDB
 const connectDB = async () => {
     try {
