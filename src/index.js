ce7ffdf3fcdaee516e2b0c192bf2b8fa59fec9f2
Mike <michaelgross.tn@gmail.com>
2023-10-15 20:37:09 -0400

    fix cors

--- src/index.js
+++ src/index.js
@@ -18,10 +18,11 @@ const passport = require('passport');
 require('./Passport.js');
 //Allow CORS
 
-app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader('Access-Control-Allow-Method', "GET", "POST", "PUT", "PATCH", "DELETE");
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+app.use((request, response, next)=>{
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 })
 
