029827bad141b2a6ec10dc4df0ae2c632d835d5b
Mike <michaelgross.tn@gmail.com>
2023-10-15 20:33:02 -0400

    New

--- src/index.js
+++ src/index.js
@@ -17,9 +17,7 @@ const auth = require('./Auth.js')(app)
 const passport = require('passport');
 require('./Passport.js');
 //Allow CORS
-const cors = require('cors')
 
-app.use(cors('*'));
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Method', "GET", "POST", "PUT", "PATCH", "DELETE");
