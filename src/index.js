202a6a926557666e99597b578ff1cca91f622506
Mike <michaelgross.tn@gmail.com>
2023-10-17 19:33:16 -0400

    SHIT

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
+app.use(cors(corsOptions));
 app.use((request, response, next)=>{
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -29,7 +30,7 @@ app.use((request, response, next)=>{
     response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 })
-app.use(cors(corsOptions));
+
 
 
 
