dbbbf0ea12fa918a663ae7b3702e8b84a107b369
Mike <michaelgross.tn@gmail.com>
2023-10-21 13:54:46 -0400

    newCommit

--- src/Passport.js
+++ src/Passport.js
@@ -1,57 +1,51 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const Models = require('../Models/AllModels.js');
-const passportJWT = require('passport-jwt')
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy,
+  Models = require('../Models/AllModels'),
+  passportJWT = require('passport-jwt');
 
-let Users = Models.Users;
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
+let Users = Models.Users,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: "Username",
-            passwordField: "Password",
-        },
-        async (Username, Password, callback) => {
-            console.log(`${Username} ${Password}`);
-            await Users.findOne({ Username: Username })
-                .then((user) => {
-                    if (!user) {
-                        console.log('Incorrect Username');
-                        return callback(null, false, {
-                            message: 'Incorrect Username or Password'
-                        })
-                    }
-                    if (!user.validatePassword(Password)) {
-                        console.log('Incorrect Password...')
-                        return callback(null, false, {
-                            message: 'Incorrect Password'
-                        });
-                    }
-                    console.log('Finished')
-                    return callback(null, user)
-                })
-                .catch((error) => {
-                    if (error) {
-                        console.log(error)
-                        return callback(error);
-                    }
-                })
+  new LocalStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password',
+    },
+    async (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      await Users.findOne({ Username: username })
+      .then((user) => {
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, {
+            message: 'Incorrect username or password.',
+          });
         }
-    )
+        console.log('finished');
+        return callback(null, user);
+      })
+      .catch((error) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+      })
+    }
+  )
 );
 
+
 passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-},
-    async (jwtPayload, callback) => {
-        return await Users.findById(jwtPayload._id)
-            .then((user) => {
-                return callback(null, user);
-            })
-            .catch((error) => {
-                return callback(error)
-            })
-}))
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+  return await Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
+}));
--- src/index.js
+++ src/index.js
@@ -13,10 +13,21 @@ dotenv.config({path: './.env'})
 const app = express();
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+let allowedOrigins = ["Access-Control-Allow-Origin: *"];
 
-require('./Auth.js')(app)
-require('./Passport.js');
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./Passport');
 
 
 
