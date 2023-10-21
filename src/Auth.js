9514233ef1b4270725de94e0501f2557bdf6b13f
Mike <michaelgross.tn@gmail.com>
2023-10-21 15:54:09 -0400

    newCommitsssss

--- src/Auth.js
+++ src/Auth.js
@@ -1,9 +1,9 @@
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
+  passport = require('passport');
 
-
-const passport = require('./Passport'); // Your local passport file
+require('./Passport'); // Your local passport file
 
 
 let generateJWTToken = (user) => {
